c88b6b49bddcfe4c2994cfbc6e93291da783bc4a
Nwagu Victor <nwaguvictor100@gmail.com>
2025-05-04 03:47:52 +0100

    fx: making transaction wallets optional

--- src/database/migrations/20250429214942_create_transactions_table.ts
+++ src/database/migrations/20250429214942_create_transactions_table.ts
@@ -4,8 +4,8 @@ export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('transactions', (table) => {
     table.uuid('id').primary().notNullable();
     table.float('amount').notNullable();
-    table.string('sender_wallet').notNullable();
-    table.string('receiver_wallet').notNullable();
+    table.string('sender_wallet').nullable();
+    table.string('receiver_wallet').nullable();
     table.enum('type', ['FUND', 'TRANSFER', 'WITHDRAW']).notNullable();
     table.enum('status', ['PENDING', 'SUCCESS', 'FAILED']).defaultTo('PENDING');
     table.string('reference').unique().notNullable();
